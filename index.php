12cfa96909c16f5f3048e4f5db185ee5f27ab852
locideran <gabriel-masc@hotmail.com>
2024-10-03 13:08:13 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Criando primeiro arquivo</title>
+</head>
+<body>	
+	<?php 
+	require_once 'sistema/configuracao.php';
+	include_once 'funcoes.php';
+	echo "<h1>Arquivo index</h1>";
+	?>
+</body>
+</html>
---
+++ sistema/configuracao.php
@@ -0,0 +1,3 @@
+<?php 
+echo "<p>Arquivo de configuração</p>";
+?>
